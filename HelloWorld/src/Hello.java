52e93acf26c74b1288d7d7e8d3e22cd966194865
Nadeem Baig <n.baig90@gmail.com>
2021-06-27 14:12:31 -0500

    int data type

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,5 +2,12 @@ public class Hello {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    int myFirstNumber = 5;
+    int mySecondNumber = 12;
+    int myThirdNumber = 6;
+    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+    System.out.println(myTotal); //sout for shortcut
+    int myLastOne = 1000 - myTotal;
+    System.out.println(myLastOne);
   }
 }
